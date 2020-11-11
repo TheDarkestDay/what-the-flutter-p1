2c65ec4066f360e53b6c70d72cbbe98f1efa34c9
Aleksandr Brenchev <abrenchev@griddynamics.com>
2020-11-11 18:35:26 +0100

    task-01-components. Setup base boilerplate for task-01

---
+++ lib/home-page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+/**
+ * Please, use this class for your solution
+ * 
+ */
+class HomePage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Text('Welcome home!');
+  }
+}
