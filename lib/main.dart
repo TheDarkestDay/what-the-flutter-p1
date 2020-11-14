e81ceb47ae3531f81ad4ccc18c819bc57c55945f
Aleksandr Brenchev <abrenchev@griddynamics.com>
2020-11-14 20:00:43 +0100

    Added entry point for routing task

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,10 @@ class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    /**
+     * Define your routes via MaterialApp widget configuration
+     * 
+     */
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
