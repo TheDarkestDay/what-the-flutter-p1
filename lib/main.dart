c788248c32abadf68da837be5b1b24a59b67fd42
Aleksandr Brenchev <abrenchev@griddynamics.com>
2020-11-15 21:02:16 +0100

    task-04-state-management. Added boilerplate for task-04

--- lib/main.dart
+++ lib/main.dart
@@ -6,35 +6,48 @@ void main() {
   runApp(App());
 }
 
+class AppStateWrapper extends StatelessWidget {
+  final Widget child;
+
+  AppStateWrapper({this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    /**
+     * Define your providers and inject them into widgets tree here
+     * 
+     */
+    return child;
+  }
+}
+
 class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    /**
-     * Define your routes via MaterialApp widget configuration
-     * 
-     */
-    return MaterialApp(
-      title: 'Flutter Demo',
-      routes: {
-        '/': (context) => WordsForTodayPage(),
-        '/dictionary': (context) => DictionaryPage(),
-      },
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return AppStateWrapper(
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        routes: {
+          '/': (context) => WordsForTodayPage(),
+          '/dictionary': (context) => DictionaryPage(),
+        },
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+          // This makes the visual density adapt to the platform that you run
+          // the app on. For desktop platforms, the controls will be smaller and
+          // closer together (more dense) than on mobile platforms.
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
       ),
     );
   }
