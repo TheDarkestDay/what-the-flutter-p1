ef3072476e4ca8980c33243e1a1f7c3a5c854dac
Aleksandr Brenchev <abrenchev@griddynamics.com>
2020-11-23 18:24:35 +0100

    task-01-components. Added more elaborated guidance

--- lib/home-page.dart
+++ lib/home-page.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
 
 /**
- * Please, use this class for your solution
+ * Please, use this class for your solution.
+ * 
+ * There are several paths for your to follow 
+ * in order to complete this exercise.
+ * 
+ * 1. You can simply put all of your logic inside one huge StatefullWidget
+ *  and be completely happy with that
+ * 
+ * - or -
+ * 
+ * 2. You can separate your concerns a little and try to split your UI
+ * into pieces and find some good places to apply both StatelessWidget and
+ * StatefullWidget
+ * 
+ * If you feel a bit off the track or you fell some lack of context
+ * feel free to consult with the offical Flutter introduction to
+ * widgets:
+ * https://flutter.dev/docs/development/ui/widgets-intro
  * 
  */
 class HomePage extends StatelessWidget {
