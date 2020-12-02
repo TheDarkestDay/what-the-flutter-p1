f0180d8d02f8a6b179449f6ceb887a8a2470cbf2
Aleksandr Brenchev <abrenchev@griddynamics.com>
2020-12-02 18:56:08 +0100

    Added Sql helper usage

--- lib/setup-database.dart
+++ lib/setup-database.dart
@@ -1,10 +1,11 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import 'package:what_the_flutter_p1/sql-query-helper.dart';
+import 'package:what_the_flutter_p1/sql-query.dart';
 
 Future<Database> setupDatabase() async {
   return openDatabase(join(await getDatabasesPath(), 'active-vocabulary.db'),
       onCreate: (db, version) {
-    return db.execute(
-        'CREATE TABLE Words(id INTEGER PRIMARY KEY autoincrement, text TEXT, usageCount INTEGER)');
+    return db.execute(SqlQueryHelper.getQuery(SqlQuery.createWordsTable));
   }, version: 1);
 }
